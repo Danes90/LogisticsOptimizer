c307faa146bc71764ceec7eb5a215c7d8fa62564
danielhegyi <hdani1990@gmail.com>
2026-09-11 14:22:34 +0200

    fix issues

---
+++ Logistics.Domain/Entities/PlacementResult.cs
@@ -0,0 +1,35 @@
+﻿namespace Logistics.Domain.Entities;
+
+public sealed class PlacementResult
+{
+    public Guid Id { get; private set; }
+
+    public Guid LoadPlanId { get; private set; }
+
+    public Guid PalletId { get; private set; }
+
+    public int X { get; private set; }
+
+    public int Y { get; private set; }
+
+    public bool Rotated { get; private set; }
+
+    private PlacementResult()
+    {
+    }
+
+    public PlacementResult(
+        Guid loadPlanId,
+        Guid palletId,
+        int x,
+        int y,
+        bool rotated)
+    {
+        Id = Guid.NewGuid();
+        LoadPlanId = loadPlanId;
+        PalletId = palletId;
+        X = x;
+        Y = y;
+        Rotated = rotated;
+    }
+}
